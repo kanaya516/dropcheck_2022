6b78ee392cf55ee4b3cdf04907533aa338a12e1a
Koichiro-Kanaya <abcs516tk1@gmail.com>
2022-06-08 18:35:56 +0900

    small fix

--- dropcheck.sh
+++ dropcheck.sh
@@ -5,4 +5,7 @@ if [ $(ifconfig $myairport | grep status | awk '{print $2}') = 'active' ]; then
    networksetup -setairportpower $myairport off
    echo $'\e[33mdisable Wi-Fi interface...\e[0m'
 fi
+
+open -a "Google Chrome" http://ipv4.google.com http://ipv6.google.com
+
 sudo tmuxp load dropcheck_tmux.yml
